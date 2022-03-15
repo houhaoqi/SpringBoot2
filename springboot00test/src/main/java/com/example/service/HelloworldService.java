040a2b309a0ac625da2f7b2d4e6fc501c9a340cd
houhaoqi <359965863@qq.com>
2022-03-15 14:34:57 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	springboot01aliyun_ioc_aop/.mvn/wrapper/maven-wrapper.jar
    #	springboot01aliyun_ioc_aop/pom.xml
    #	springboot01aliyun_ioc_aop/src/main/java/com/haoqi/AOP/advice/PermissionSecondAdvice.java
    #	springboot01aliyun_ioc_aop/src/main/java/com/haoqi/AOP/controller/PermissionsController.java
    #	springboot01ioc_aop/src/main/resources/application.properties
    #	springboot102_junit/.mvn/wrapper/maven-wrapper.jar
    #	springboot102_junit/src/main/resources/application.properties
    #	springboot102_junit/target/classes/application.properties
    #	springboot103_mybatis/.mvn/wrapper/maven-wrapper.jar
    添加springboot104_ssmp的demo

---
+++ springboot00test/src/main/java/com/example/service/HelloworldService.java
@@ -0,0 +1,36 @@
+package com.example.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author haoqi
+ * @Date 2022/3/15 - 14:09
+ */
+
+@Controller
+@EnableAutoConfiguration
+public class HelloworldService {
+
+    //@RestController == @Controller + @ResponseBody：
+    //  类上添加@RestController springMvcUrl接口映射都返回json格式
+    //  方法上添加@RestController 返回对应json字符页面
+    //  Rest风格通过http协议传输json数据格式
+    //
+    //  @Controller  springMvcUrl接口映射，默认返回页面跳转，
+    //需要搭配@ResponseBody注解返回json格式数据
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello haoqi!这是json字符串，传输到页面中去！";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloworldService.class,args);
+    }
+}
