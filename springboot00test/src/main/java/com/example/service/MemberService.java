f077fc2a1b7ce67e9dcbc3a224fdbff8e83c4de7
houhaoqi <359965863@qq.com>
2022-03-16 10:23:12 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	springboot01aliyun_ioc_aop/.mvn/wrapper/maven-wrapper.jar
    #	springboot01aliyun_ioc_aop/pom.xml
    #	springboot01aliyun_ioc_aop/src/main/java/com/haoqi/AOP/advice/PermissionSecondAdvice.java
    #	springboot01aliyun_ioc_aop/src/main/java/com/haoqi/AOP/controller/PermissionsController.java
    #	springboot01ioc_aop/src/main/resources/application.properties
    #	springboot102_junit/.mvn/wrapper/maven-wrapper.jar
    #	springboot102_junit/src/main/resources/application.properties
    #	springboot102_junit/target/classes/application.properties
    #	springboot103_mybatis/.mvn/wrapper/maven-wrapper.jar
    添加springboot104_ssmp的demo

---
+++ springboot00test/src/main/java/com/example/service/MemberService.java
@@ -0,0 +1,45 @@
+package com.example.service;
+
+import com.example.entity.YmlEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author haoqi
+ * @Date 2022/3/15 - 14:28
+ */
+@RestController
+@EnableAutoConfiguration
+public class MemberService {
+
+    //读取propertie文件配置
+    @Value("${test.name}")
+    private String name;
+    @Value("${test.age}")
+    private String age;
+    //通过YmlEntity类中@ConfigurationProperties(prefix = "test")注解读取配置文件内容
+    @Autowired
+    private YmlEntity ymlEntity;
+    @RequestMapping("/ymlEntity")
+    public String ymlEntity(){
+        return ymlEntity.toString();
+    }
+
+    @RequestMapping("/member")
+    public String member(){
+        return "member页面";
+    }
+
+    @GetMapping("/properties")
+    public String properties(){
+        return name+" -- "+age;
+    }
+
+
+
+}
